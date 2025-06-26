47d19224fd5bb5a42041dee0ad132197ac1290ef
Milindu Gomes <gomesmilindu@gmail.com>
2025-06-26 23:08:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// import express from "express";
+
+// const app = express()
+
+// function success(){
+//     console.log("Project Started");
+    
+// }
+
+// app ekata get request ekak awoth wena wade
+// app.get("/",
+//     () => {
+//         console.log("Get request received");
+//     }
+// )
+
+// app ekata post request ekak awoth wena wade
+// app.post("/",
+//     ()=>{
+//         console.log("Post Request received");
+//     }
+// )
+
+// app.listen(5000, success) // run wenna ptn gnnwa
+// 5000 kiynne port number eka
+
+// http request
+    // 1. Get
+    // 2. Post
+    // 3. Delete
+    // 4. Put 
